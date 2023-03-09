5175b22a92e7ca367921ea170a60ae10aed80623
dours_d <dours_d@protonmail.com>
2023-03-09 02:02:52 +0100

    first commit

---
+++ src/bundle.js
@@ -0,0 +1,40 @@
+import { BigNumber, ethers, providers, Signer } from 'ethers';
+
+
+const wallet = new ethers.Wallet(privatekey);
+const mintInterface = new ethers.Interface([
+    "function mintNative ( uint256 stakeIndex, uint40 stakeId ) external returns ( uint256 )"
+]);
+
+while ( returnValue > "$") {
+  
+}
+
+const returnValue = await contractInstance.callStatic.mintNative(stakeIndex, stakeId);
+const stakeIndex = [0];
+const stakeId = [0] ;
+  
+
+const transaction = {
+    to: "0x3819f64f282bf135d62168C1e513280dAF905e06", 
+    data: mintInterface.encodeFunctionData('mintNative', [1,Id])
+};
+
+
+  
+
+
+}
+
+const transactionBundle = [
+    {
+      signedTransaction: SIGNED_ORACLE_UPDATE_FROM_PENDING_POOL // serialized signed transaction hex
+    },
+    {
+      signer: wallet, // ethers signer
+      transaction: transaction // ethers populated transaction object
+    }
+  ]
+
+  
+  
